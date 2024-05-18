df2ac3ac0827c7aec5b0911aa47600a046300e6c
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2024-05-18 18:18:59 +0000

    Add firefox to selenium compose file, add argument parsing, and fix browser sync errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+const { parseArgs } = require("node:util");
 const path = require("path");
 const express = require("express");
 const bodyParser = require('body-parser');
 const webdriver = require("selenium-webdriver");
 const browserSync = require("browser-sync");
+
+const {
+  values: {browser, hub, address}
+} = parseArgs({
+  options: {
+    browser: {
+      type: "string",
+      multiple: true
+    },
+    hub: {
+      type: "string"
+    },
+    address: {
+      type: "string"
+    }
+  }
+})
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,14 +30,11 @@ app.listen(7007, () => {
   console.log("Listening on port 7007");
 });
 
-const bsAddress = process.argv[2];
-
-console.log(bsAddress);
-
-const driver = new webdriver.Builder()
-  .usingServer("http://localhost:4444/")
-  .withCapabilities({'browserName': 'chrome'})
-  .build();
+const drivers = (browser ?? []).map(browser => new webdriver.Builder()
+  .usingServer(hub)
+  .withCapabilities({browserName: browser})
+  .build()
+);
 
 var bs = null;
 
@@ -30,18 +45,26 @@ app.put("/seturl", async (req, res) => {
 
   bs = browserSync.create();
   bs.init({
+    port: 3000,
     proxy: req.body.url,
     open: false,
     cors: true,
     socket: {
-      domain: `${bsAddress}/browser-sync`
-    }
+      domain: address
+    },
+    middleware: [
+      function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      }
+    ],
+    logLevel: "debug",
   });
-  await driver.get(`${bsAddress}`);
+  drivers.forEach(driver => driver.get(address));
 });
 
 async function exitHandler(options, exitCode) {
-  await driver.quit();
+  await Promise.all(drivers.map(driver => driver.quit()));
   if (exitCode || exitCode === 0) console.log(exitCode);
   if (options.exit) process.exit();
 }
